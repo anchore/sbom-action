091157ad1ab06e6ea9900ce5b1d589b3175a1c8d
Keith Zantow <kzantow@gmail.com>
2021-12-13 08:48:41 -0500

    Bump Syft to 0.32.0
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- dist/runSyftAction/index.js
+++ dist/runSyftAction/index.js
@@ -16810,7 +16810,7 @@ const path_1 = __importDefault(__nccwpck_require__(1017));
 const stream_1 = __importDefault(__nccwpck_require__(2781));
 const GithubClient_1 = __nccwpck_require__(8552);
 exports.SYFT_BINARY_NAME = "syft";
-exports.SYFT_VERSION = "v0.26.0";
+exports.SYFT_VERSION = "v0.32.0";
 const PRIOR_ARTIFACT_ENV_VAR = "ANCHORE_SBOM_ACTION_PRIOR_ARTIFACT";
 /**
  * Tries to get a unique artifact name or otherwise as appropriate as possible
