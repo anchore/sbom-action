66a41f306ccf55a2adff630d41b6864da6fbbfc5
laurentsimon <laurentsimon@google.com>
2023-01-30 17:37:04 +0000

    update
    Signed-off-by: laurentsimon <laurentsimon@google.com>

---
+++ internal/sbom-wrapper/generate-layout.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#find sboms/ -maxdepth 2 -regex 'sboms/sbom-action-.*/*.json' > FILES
+sudo find /etc/ -maxdepth 2 -regex '/etc/sbom-action-.*/*.json' | tee ./FILES
+
+attestations=()
+n=$(wc -l <./FILES)
+i=1
+while IFS= read -r line; do
+    file="$line"
+
+    echo "SBOM file: $file"
+    hash=$(sha256sum "$file" | awk '{print $1}')
+    subject_name=$(basename "$(readlink -m "$file")")
+    read -r -d '' entry <<- EOM
+    {
+        "name": "$subject_name",
+        "digest":
+        { 
+            "sha256": "$hash"
+        }
+    }
+EOM
+    if [[ $i -eq $n ]]; then
+        attestations+=("$entry")
+    else
+        attestations+=("$entry,")
+    fi
+    
+    i=$((i+1))
+done < FILES
+
+cat <<EOF >DATA
+{
+    "version": 1,
+    "attestations":
+    [
+        {
+            "name": "attestation.intoto",
+            "subjects": 
+            [
+                ${attestations[@]}
+            ]
+        }
+    ]
+}
+EOF
+cat DATA
+jq <DATA
