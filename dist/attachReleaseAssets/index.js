e3661879ac7d1b05ce8552a97cd59425cf510b86
kzantow <kzantow@users.noreply.github.com>
2024-03-07 07:09:49 +0000

    chore(deps): update Syft to v1.0.1
    Signed-off-by: GitHub <noreply@github.com>

--- dist/attachReleaseAssets/index.js
+++ dist/attachReleaseAssets/index.js
@@ -23385,7 +23385,7 @@ function wrappy (fn, cb) {
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.VERSION = void 0;
-exports.VERSION = "v0.105.0";
+exports.VERSION = "v1.0.1";
 
 
 /***/ }),
